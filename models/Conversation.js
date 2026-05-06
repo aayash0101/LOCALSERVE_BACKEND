3c5831b3a592a1d3fb5ea8669df077132a222ff4
Aayash <dangaayash@gmail.com>
2026-05-06 13:38:16 +0545

    Conversation/Chat Model

---
+++ models/Conversation.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+
+const conversationSchema = new mongoose.Schema({
+  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  lastMessage: { type: String, default: '' },
+}, { timestamps: true });
+
+export default mongoose.model('Conversation', conversationSchema);
