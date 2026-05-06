e835ff38138488fb3983d7222d75ae6de1d3dfef
Aayash <dangaayash@gmail.com>
2026-05-06 13:59:29 +0545

    auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,7 +10,7 @@ export const register = async (req, res) => {
   }
 
   const user = await User.create({ name, email, password, role });
-
+ 
   generateToken(res, user._id);
 
   res.status(201).json({
