a69e8e366dddff8f9a7e3e082a626bce6b3546ce
Aayash <dangaayash@gmail.com>
2026-05-07 11:15:13 +0545

    cloudinary utils

---
+++ utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadToCloudinary = (fileBuffer, folder) => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      { folder: `localserve/${folder}` },
+      (error, result) => {
+        if (error) reject(error);
+        else resolve(result);
+      }
+    );
+    stream.end(fileBuffer);
+  });
+};
+
+export const deleteFromCloudinary = async (publicId) => {
+  await cloudinary.uploader.destroy(publicId);
+};
+
+export default cloudinary;
